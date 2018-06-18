2be69d64807df1184177f2d6d8bb353cdbaa95b8
Maxime Veber <nek.dev@gmail.com>
2018-06-18 19:32:14 +0200

    Add missing types

--- Box2D/Testbed/webpack.config.ts
+++ Box2D/Testbed/webpack.config.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as webpack from "webpack";
-import * as CleanWebpackPlugin from "clean-webpack-plugin";
+// import * as CleanWebpackPlugin from "clean-webpack-plugin";
+import * as CleanWebpackPlugin from 'clean-webpack-plugin';
 
 const distDir = path.resolve(__dirname, "../../dist/Testbed");
 
